c7811719335a85dd10ab613b16eb60e632ad440e
stoimenoff <stoimenoffjr@gmail.com>
2016-02-11 17:06:16 +0200

    Rename Week11/monday to downloadImage

--- Week11/src/downloadImage/DownloadImage.java
+++ Week11/src/downloadImage/DownloadImage.java
@@ -1,4 +1,4 @@
-package monday;
+package downloadImage;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
--- Week11/src/downloadImage/Tests.java
+++ Week11/src/downloadImage/Tests.java
@@ -1,4 +1,4 @@
-package monday;
+package downloadImage;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
