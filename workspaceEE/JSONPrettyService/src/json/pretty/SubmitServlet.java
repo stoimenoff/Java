d2d74a2ed8454bd23940c4d2e017ab2ec00de424
stoimenoff <stoimenoffjr@gmail.com>
2016-02-14 00:55:30 +0200

    JSON Pretty Service added

---
+++ workspaceEE/JSONPrettyService/src/json/pretty/SubmitServlet.java
@@ -0,0 +1,61 @@
+package json.pretty;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+/**
+ * Servlet implementation class SubmitServlet
+ */
+@WebServlet("/Submit")
+public class SubmitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SubmitServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String json = request.getParameter("json");
+
+		if (json != null) {
+			try {
+				Gson gson = new GsonBuilder().setPrettyPrinting().create();
+				JsonParser jp = new JsonParser();
+				JsonElement je = jp.parse(json);
+				String prettyJson = gson.toJson(je);
+				response.getWriter().write(prettyJson);
+			} catch (Exception e) {
+				response.getWriter().write("Invalid JSON !");
+			}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
